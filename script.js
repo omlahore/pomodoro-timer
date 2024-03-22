d62ad75ab38cc4c9946363aa2980e0fc65728a06
Om Lahore <119071446+omlahore@users.noreply.github.com>
2024-03-23 03:38:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+// variables
+let workTime = 25;
+let breakTime = 5;
+let seconds = "00";
+let timerInterval;
+
+// display
+window.onload = () => {
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = seconds;
+}
+
+// start timer
+function start() {
+    // change button
+    document.getElementById('start').style.display = "none";
+    document.getElementById('reset').style.display = "block";
+
+    // reset seconds
+    seconds = 59;
+
+    // start work timer
+    startWorkTimer();
+}
+
+// start work timer
+function startWorkTimer() {
+    let workMinutes = workTime - 1;
+
+    timerInterval = setInterval(() => {
+        // change the display
+        document.getElementById('minutes').innerHTML = workMinutes;
+        document.getElementById('seconds').innerHTML = seconds;
+
+        // start countdown
+        seconds--;
+
+        if (seconds < 0) {
+            workMinutes--;
+            seconds = 59;
+
+            if (workMinutes < 0) {
+                clearInterval(timerInterval);
+                startBreakTimer();
+            }
+        }
+    }, 1000); // 1000 = 1s
+}
+
+// start break timer
+function startBreakTimer() {
+    clearInterval(timerInterval); // stop the work timer
+    let breakMinutes = breakTime - 1; // Initialize break minutes
+
+    // reset seconds
+    seconds = 59;
+
+    timerInterval = setInterval(() => {
+        // change the display
+        document.getElementById('minutes').innerHTML = breakMinutes;
+        document.getElementById('seconds').innerHTML = seconds;
+
+        // start countdown
+        seconds--;
+
+        if (seconds < 0) {
+            breakMinutes--;
+            seconds = 59;
+
+            if (breakMinutes < 0) {
+                clearInterval(timerInterval);
+                resetTimer();
+            }
+        }
+    }, 1000); // 1000 = 1s
+}
+
+// toggle between work and break buttons
+function toggleWorkBreak() {
+    const workButton = document.getElementById('workButton');
+    const breakButton = document.getElementById('breakButton');
+
+    workButton.classList.toggle('active');
+    breakButton.classList.toggle('active');
+
+    if (workButton.classList.contains('active')) {
+        resetTimer();
+        startWorkTimer();
+    } else {
+        resetTimer();
+        startBreakTimer();
+    }
+}
+
+// Reset timer
+function resetTimer() {
+    clearInterval(timerInterval); // Stop the timer interval
+    // Reset timer display to initial work time
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = "00";
+    // Reset button display
+    document.getElementById('start').style.display = "block";
+    document.getElementById('reset').style.display = "none";
+}
